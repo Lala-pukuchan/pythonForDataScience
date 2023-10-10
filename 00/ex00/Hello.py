2c73ec8a356b47463ba6c8186e9d3aacca79db76
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-10-10 11:24:21 +0900

    00-ex00

---
+++ 00/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "Japan!")
+ft_set = {"Hello", "Tokyo!"}
+ft_dict = {"Hello" : "42Tokyo!"}
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
