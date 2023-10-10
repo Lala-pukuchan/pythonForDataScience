671313646e20f1c77f0241253b9055334266337d
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-10-10 15:15:23 +0900

    00-ex03

---
+++ 00/ex03/NULL_not_found.py
@@ -0,0 +1,16 @@
+import math
+
+def NULL_not_found(object: any) -> int:
+	if object is None:
+		print(f"Nothing: {object} {type(object)}")
+	elif isinstance(object, float) and math.isnan(object):
+		print(f"Cheese: {object} {type(object)}")
+	elif isinstance(object, bool) and object == False:
+		print(f"Fake: {object} {type(object)}")
+	elif object == "":
+		print(f"Empty: {type(object)}")
+	elif object == 0:
+		print(f"Zero: {object} {type(object)}")
+	else:
+		print("Type not Found")
+	return 1
