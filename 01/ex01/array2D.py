aa971aec33e4a9883b4cc8d1b5cf2d75a8ccc369
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-10-13 10:28:29 +0900

    01-ex01

--- 01/ex01/array2D.py
+++ 01/ex01/array2D.py
@@ -2,11 +2,22 @@ def slice_me(family: list, start: int, end: int) -> list:
     '''
     If list is not a list, raise a ValueError.
     Slice a list of lists.
-    
     '''
     try:
         if not isinstance(family, list):
-            raise ValueError("The 'family' parameter must be a list.")
+            raise AssertionError("The 'family' parameter must be a list.")
+        for item in family:
+            if not isinstance(item, list):
+                raise AssertionError(
+                    "The 'family' parameter must be a list of lists.")
+            if len(item) != len(family[0]):
+                raise AssertionError(
+                    "All lists in the 'family' parameter must have " +
+                    "the same length.")
+        print(f"My shape is : ({len(family)}, {len(family[0])})")
+        sliced = family[start:end]
+        print(f"My new shape is : ({len(sliced)}, {len(sliced[0])})")
+        return family[start:end]
 
     except AssertionError as error:
         print(AssertionError.__name__ + ":", error)
