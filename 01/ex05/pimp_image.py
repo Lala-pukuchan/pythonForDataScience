5e74b1243257ecf16d0e9df59918bffdc91ba608
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-10-16 10:41:58 +0900

    01-ex05

--- 01/ex05/pimp_image.py
+++ 01/ex05/pimp_image.py
@@ -19,7 +19,8 @@ def ft_red(array) -> array:
     and sets the red channel's value to 0.
     '''
     img = array.copy()
-    img[:, :, 0] = 0
+    img[:, :, 1] = 0
+    img[:, :, 2] = 0
     Image.fromarray(img).show()
     return img
 
@@ -30,7 +31,8 @@ def ft_green(array) -> array:
     and sets the green channel's value to 0.
     '''
     img = array.copy()
-    img[:, :, 1] = 0
+    img[:, :, 0] = 0
+    img[:, :, 2] = 0
     Image.fromarray(img).show()
     return img
 
@@ -41,7 +43,8 @@ def ft_blue(array) -> array:
     and sets the blue channel's value to 0.
     '''
     img = array.copy()
-    img[:, :, 2] = 0
+    img[:, :, 0] = 0
+    img[:, :, 1] = 0
     Image.fromarray(img).show()
     return img
 
@@ -53,10 +56,15 @@ def ft_grey(array) -> array:
     to the average of the three.
     '''
     img = array.copy()
-    gray = (img[:, :, 0] + img[:, :, 1] + img[:, :, 2]) / 3
-    img[:, :, 0] = gray
-    img[:, :, 1] = gray
-    img[:, :, 2] = gray
+
+    red_channel = array[:, :, 0] / 3
+    green_channel = array[:, :, 1] / 3
+    blue_channel = array[:, :, 2] / 3
+    grey_channel = red_channel + green_channel + blue_channel
+    img[:, :, 0] = grey_channel
+    img[:, :, 1] = grey_channel
+    img[:, :, 2] = grey_channel
+
     Image.fromarray(img).show()
     return img
 
