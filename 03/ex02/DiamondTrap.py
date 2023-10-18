2bd0a773f9168321d221d9cbab52c5f930dafafe
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-10-18 16:22:36 +0900

    03-ex01

---
+++ 03/ex02/DiamondTrap.py
@@ -0,0 +1,30 @@
+from S1E7 import Baratheon, Lannister
+
+
+class King(Baratheon, Lannister):
+    '''
+    King class
+    '''
+    def __init__(self, first_name, is_alive=True):
+        '''
+        if is_alive is not provided, it should be set to True by default
+        '''
+        super().__init__(first_name, is_alive)
+
+    def set_eyes(self, color):
+        '''
+        set the eyes color
+        '''
+        self.eyes = color
+
+    def set_hairs(self, color):
+        '''
+        set the hairs color
+        '''
+        self.hairs = color
+
+    def get_eyes(self):
+        return self.eyes
+
+    def get_hairs(self):
+        return self.hairs
---
+++ 03/ex02/S1E9.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """
+    take first_name and is_alive as parameters
+    first_name is a string
+    is_alive is a boolean that defaults to True
+    """
+    @abstractmethod
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self, is_alive):
+        pass
+
+
+class Stark(Character):
+    """Stark class is inherited from the Character class"""
+    def __init__(self, first_name, is_alive=True):
+        """if is_alive is not provided, it should be set to True by default"""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """die method sets is_alive to False"""
+        self.is_alive = False
